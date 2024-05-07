ed8669973894665a5f31d2734a5c2cc750964aec
xlgelingen <xuliucg@gmail.com>
2024-05-07 21:32:25 +0800

    feat: add filter interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 declare const module: any;
 
 async function bootstrap() {
@@ -8,6 +10,12 @@ async function bootstrap() {
 
   // 启动全局字段校验，保证请求接口字段校验正确。
   app.useGlobalPipes(new ValidationPipe());
+
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // 全局注册拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
 
